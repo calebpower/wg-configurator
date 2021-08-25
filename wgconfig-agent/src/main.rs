039ffb72cae3d97e05fc3af34836b9298f872774
Caleb L. Power <cpower@axonibyte.com>
2021-08-25 09:18:22 -0400

    applied config retrieval + checksum mechanism

--- wgconfig-agent/src/main.rs
+++ wgconfig-agent/src/main.rs
@@ -1,8 +1,10 @@
 extern crate crypto;
+extern crate regex;
 extern crate reqwest;
 
 use self::crypto::digest::Digest;
 use self::crypto::sha2::Sha256;
+use regex::Regex;
 use std::fs;
 use std::path::Path;
 
@@ -20,7 +22,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .text()
         .await?;
 
-    println!("{}", wg_config);
+    println!("Net config:\n\n{}", wg_config);
 
     let mut hasher = Sha256::new();
     hasher.input_str(&wg_config);
@@ -31,14 +33,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let disk_file = "./wg-test.conf";
     let mut current = Path::new(&disk_file).exists();
 
+    let private_key = "MY_PRIVKEY";
+
     if current {
       let contents = fs::read_to_string(&disk_file)
           .expect("Something went wrong reading the file");
 
-      println!("{}", contents);
+      println!("Disk config:\n\n{}", contents);
+
+      let mut regstr = "PrivateKey\\s*=\\s*".to_owned();
+      regstr.push_str(&private_key);
+      regstr.push_str("\n");
+
+      let re = Regex::new(&regstr).unwrap();
+      let transformed = re.replace_all(&contents, "PrivateKey = {{ PRIVATE_KEY }}\n");
+      println!("Redacted disk config:\n\n{}", &transformed);
 
       hasher = Sha256::new();
-      hasher.input_str(&contents);
+      hasher.input_str(&transformed);
       let newhex = hasher.result_str();
       println!("newhash = {}", newhex);
 
@@ -46,7 +58,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     if !current {
-      fs::write(&disk_file, &wg_config)?;
+      let mut replacement = "PrivateKey = ".to_owned();
+      replacement.push_str(&private_key);
+      replacement.push_str("\n");
+
+      let re = Regex::new("PrivateKey\\s*=.*\n").unwrap();
+      let transformed = re.replace_all(&wg_config, &replacement);
+
+      println!("Applied net config:\n\n{}", &transformed);
+
+      fs::write(&disk_file, &*transformed)?;
     }
 
     println!("Current = {}", current);
